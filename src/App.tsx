a01c1ff274b29de495fcf44b0b54b06c7f1ba004
Leonardo Burati <leonardoburati@hotmail.com>
2021-03-25 19:21:25 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, StyleSheet, StatusBar } from 'react-native';
 
 import Routes from './routes';
+import AppProvider from './hooks';
 
 function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#10193a" />
-      <View style={ styles.container }>
-        <Routes />
-      </View>
+      <AppProvider>
+        <StatusBar barStyle="light-content" backgroundColor="#10193a" />
+        <View style={styles.container}>
+          <Routes />
+        </View>
+      </AppProvider>
     </NavigationContainer>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,14 +9,21 @@ const Play = createStackNavigator();
 
 const Routes = () => (
   <Play.Navigator screenOptions={{
-    headerShown: false,
+    // headerShown: false,
     cardStyle: {
       backgroundColor: '#10193a'
-     }
+    }
   }}>
-    <Play.Screen name='Home' component={Home} />
-    <Play.Screen name='Game' component={Game} />
-    <Play.Screen name='Leaderboard' component={Leaderboard} />
+    <Play.Screen name='Home' component={Home} options={{
+      headerShown: false,
+    }} />
+    <Play.Screen name='Game' component={Game} options={{
+      headerShown: false,
+    }} />
+    <Play.Screen name='Leaderboard' component={Leaderboard} options={{
+      headerTintColor: '#fff',
+      headerStyle: { backgroundColor: '#10193a' }
+    }} />
   </Play.Navigator>
 );
 
