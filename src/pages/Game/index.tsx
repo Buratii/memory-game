393711371ddc8327ea3f962eca15340addc152c4
Leonardo Burati <leonardoburati@hotmail.com>
2021-03-25 19:19:38 -0300

    feat: create game page

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,18 +1,85 @@
-import React, { ReactNode } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRoute } from '@react-navigation/native';
 
-import { Text } from 'react-native';
+import Card from '../../components/Card';
+import Modal from '../../components/Modal';
+import { useCard } from '../../hooks/card';
+import cardsData from '../../utils/cardsData';
 
-import { Container } from './styles';
-
-interface GameProps {
-  children: ReactNode;
+interface CardDistributionProps {
+  item: Card;
+  index: number;
 }
 
-function Game({ children }: GameProps) {
+import { Container, Content, UserWrapper, UserTextData, CardDistribution } from './styles';
+
+const shuffleCards = cardsData.sort(() => Math.random() - 0.5);
+
+function Game() {
+  const { attempts, registerPlayer } = useCard();
+  const { params }: any = useRoute();
+
+  const [visibleCard, setVisibleCard] = useState<number[]>([]);
+  const [matchedCards, setMatchedCards] = useState<number[]>([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  const checkItems = (firstIndex: number, secondIndex: number) => {
+    if (
+      firstIndex !== secondIndex &&
+      shuffleCards[firstIndex].pair === shuffleCards[secondIndex].pair
+    ) {
+      setMatchedCards([...matchedCards, firstIndex, secondIndex]);
+    } else {
+      setTimeout(() => {
+        this['card' + firstIndex].flip();
+        this['card' + secondIndex].flip();
+        setVisibleCard([]);
+      }, 600);
+    }
+  };
+
+  useEffect(() => {
+    const Win = async () => {
+      if (matchedCards.length > 0 && matchedCards.length === shuffleCards.length) {
+        const user = { name: params.nickname, attempts };
+        await registerPlayer(user);
+        setOpenModal(true);
+      }
+    }
+
+    Win();
+  }, [matchedCards]
+  );
+
   return (
     <Container>
-      <Text>Game</Text>
-      {children}
+      <Modal
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        setMatchedCards={setMatchedCards}
+        matchedCards={matchedCards}
+      />
+      <Content>
+        <UserWrapper>
+          <UserTextData>{params.nickname}</UserTextData>
+          <UserTextData>{attempts}</UserTextData>
+        </UserWrapper>
+        <CardDistribution
+          data={shuffleCards}
+          renderItem={({ item, index }: CardDistributionProps) => (
+            <Card
+              item={item}
+              index={index}
+              visibleCard={visibleCard}
+              setVisibleCard={setVisibleCard}
+              checkItems={checkItems}
+              matchedCards={matchedCards}
+            />
+          )}
+          keyExtractor={item => item.id}
+          numColumns={4}
+        />
+      </Content>
     </Container>
   );
 };
--- src/pages/Game/styles.ts
+++ src/pages/Game/styles.ts
@@ -1,5 +1,26 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  flex: 1;
+  margin: 32px 0;
+`;
 
+export const Content = styled.View`
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserWrapper = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-around;
+`;
+
+export const UserTextData = styled.Text`
+  color: #fff;
+  font-size: 24px;
+  margin-bottom: 8px;
+`;
+
+export const CardDistribution = styled.FlatList`
 `;
