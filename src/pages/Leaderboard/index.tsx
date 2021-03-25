019ec7eb4def02f1c1c40e93a431926e2a7b7aff
Leonardo Burati <leonardoburati@hotmail.com>
2021-03-25 19:20:01 -0300

    feat: create leaderboard page

--- src/pages/Leaderboard/index.tsx
+++ src/pages/Leaderboard/index.tsx
@@ -1,18 +1,51 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import { View } from 'react-native';
 
-import { Text } from 'react-native';
+import { useCard } from '../../hooks/card';
+import LeaderboardBg from '../../assets/leaderboardBg.png';
 
-import { Container } from './styles';
+import {
+  Container,
+  Ranking,
+  UserClassification,
+  PositionWrapper,
+  Position,
+  PositionText,
+  Nickname,
+  Score,
+} from './styles';
 
-interface LeaderboardProps {
-  children: ReactNode;
-}
-
-function Leaderboard({ children }: LeaderboardProps) {
+const Leaderboard = () => {
+  const { leaderboard } = useCard();
+  console.log('FIXMENOW LEADBOARD', leaderboard);
   return (
     <Container>
-      <Text>Leaderboard</Text>
-      {children}
+      <Ranking source={LeaderboardBg} />
+      {leaderboard.length > 0 ? (
+        <UserClassification showsVerticalScrollIndicator={false}>
+          {leaderboard.sort((a, b) => {
+            return a.attempts - b.attempts;
+          }).map((winner, index) => (
+            <PositionWrapper key={index}>
+              <Position place={index + 1}>
+                <PositionText>{index + 1}</PositionText>
+              </Position>
+              <Nickname>{winner.name}</Nickname>
+              <Score>{winner.attempts}</Score>
+            </PositionWrapper>
+          ))}
+        </UserClassification>
+      ) : (
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <Nickname>Nenhuma partida cadastrada</Nickname>
+        </View>
+      )
+
+      }
     </Container>
   );
 };
--- src/pages/Leaderboard/styles.ts
+++ src/pages/Leaderboard/styles.ts
@@ -1,5 +1,80 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
+
+const topScores = {
+  1: css`
+    background-color: #5cf494;
+  `,
+  2: css`
+    background-color: #82def9;
+  `,
+  3: css`
+    background-color: #c13e3d;
+  `,
+  // 4: css`
+  //   background-color: #474f67;
+  // `
+};
 
 export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  width: 100%;
+  /* justify-content: space-between; */
+  /* margin: ${height * 0.05}px 0; */
+`;
 
+export const Ranking = styled.Image`
+  width: 100%;
+  height: 36%;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #fff;
+`;
+
+export const Title = styled.Text`
+  align-self: flex-end;
+  font-size: 24px;
+
+`;
+
+export const UserClassification = styled.ScrollView`
+  width: 100%;
+  margin: 8px 0;
+`;
+
+export const PositionWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 8px;
+`;
+
+export const Position = styled.View<{ place?: number }>`
+  width: 64px;
+  height: 64px;
+  justify-content: center;
+  align-items: center;
+  ${({ place }) => place < 4 ? topScores[place ?? 4]
+    : css` background-color: #474f67; `};
+
+border-radius: 8px;
+margin: 4px 0;
+`;
+
+export const PositionText = styled.Text`
+font-size: 16px;
+color: #fff;
+`;
+
+export const Nickname = styled.Text`
+font-size: 16px;
+color: #fff;
+`;
+
+export const Score = styled.Text`
+font-size: 24px;
+color: #fff;
 `;
