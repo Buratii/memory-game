815026fdfb601e7f98b9d2e5756fea8816c964a3
Leonardo Burati <leonardoburati@hotmail.com>
2021-03-25 19:21:42 -0300

    feat: create cards body

---
+++ src/utils/cardsData.ts
@@ -0,0 +1,33 @@
+import card1 from '../assets/cards/card1.png';
+import card2 from '../assets/cards/card2.png';
+import card3 from '../assets/cards/card3.png';
+import card4 from '../assets/cards/card4.png';
+import card5 from '../assets/cards/card5.png';
+import card6 from '../assets/cards/card6.png';
+import card7 from '../assets/cards/card7.png';
+import card8 from '../assets/cards/card8.png';
+import card9 from '../assets/cards/card9.png';
+import card10 from '../assets/cards/card10.png';
+
+export default [
+  { id: 1, image: card1, pair: 1, flipped: false, match: false },
+  { id: 2, image: card1, pair: 1, flipped: false, match: false },
+  { id: 3, image: card2, pair: 2, flipped: false, match: false },
+  { id: 4, image: card2, pair: 2, flipped: false, match: false },
+  { id: 5, image: card3, pair: 3, flipped: false, match: false },
+  { id: 6, image: card3, pair: 3, flipped: false, match: false },
+  { id: 7, image: card4, pair: 4, flipped: false, match: false },
+  { id: 8, image: card4, pair: 4, flipped: false, match: false },
+  { id: 9, image: card5, pair: 5, flipped: false, match: false },
+  { id: 10, image: card5, pair: 5, flipped: false, match: false },
+  { id: 11, image: card6, pair: 6, flipped: false, match: false },
+  { id: 12, image: card6, pair: 6, flipped: false, match: false },
+  { id: 13, image: card7, pair: 7, flipped: false, match: false },
+  { id: 14, image: card7, pair: 7, flipped: false, match: false },
+  { id: 15, image: card8, pair: 8, flipped: false, match: false },
+  { id: 16, image: card8, pair: 8, flipped: false, match: false },
+  { id: 17, image: card9, pair: 9, flipped: false, match: false },
+  { id: 18, image: card9, pair: 9, flipped: false, match: false },
+  { id: 19, image: card10, pair: 10, flipped: false, match: false },
+  { id: 20, image: card10, pair: 10, flipped: false, match: false },
+]
